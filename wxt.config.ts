17933312ad6c70b2973d6ce2dd346b9e9fac77b4
MitchellV3 <mitchellvollmer@shaw.ca>
2026-05-02 07:26:58 -0600

    Add icons/auto-icons plugin

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
+  autoIcons: {
+    baseIconPath: "assets/icon.png",
+    developmentIndicator: 'overlay',
+  },
   manifest: {
     name: 'clip-dl',
     description: 'description',
